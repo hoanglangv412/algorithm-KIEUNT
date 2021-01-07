12861bc4a5c82ae0ff1f81d73ca9a59dfc8129c8
Kieu <kieungothekieu@gmail.com>
2021-01-07 09:11:57 +0700

    Sua lai 2 bai 21,22

--- bai21calSalary/Program.cs
+++ bai21calSalary/Program.cs
@@ -5,30 +5,28 @@ namespace bai21calSalary
     class Program
     {
         static int calSalaryByRecursion(int salary, int year){
-            if(year == 0) return 0;
             if(year == 1) return salary;
-            else return (salary+calSalaryByRecursion(salary,year-1)/10);
+            else return (calSalaryByRecursion(salary,year-1)+calSalaryByRecursion(salary,year-1)/10);
         }
+
         static int calSalaryBasic(int salary, int year){
             int finalSalary = 0;
-            if(year == 0) finalSalary = 0;
-            else if(year == 1) finalSalary = salary;  
+            if(year == 1) finalSalary = salary;  
             else{
                 int i = 2;
-                int beforeYear = salary;
+                finalSalary = salary;
                 while(i<=year){
-                    finalSalary =  salary + beforeYear/10;
-                    beforeYear = salary + beforeYear/10;
+                    finalSalary += finalSalary/10;
                     i++;
                 }
             }
             return finalSalary;
-
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(calSalaryByRecursion(10000000, 5));
-            Console.WriteLine(calSalaryBasic(10000000, 6));
+            Console.WriteLine("Luong de quy: " + calSalaryByRecursion(10000000, 10));
+            Console.WriteLine("Luong co ban: " + calSalaryBasic(10000000, 10));
         }
     }
 }
--- bai22calMonth/Program.cs
+++ bai22calMonth/Program.cs
@@ -8,15 +8,15 @@ namespace bai22calMonth
             if(month == 0) return money;
             else return calMonthcheck(money,rate, month-1) + calMonthcheck(money,rate, month-1)/rate;
         }
+
         static int calMonthRecursion(int money,int rate){
             int countMonth = 0;
-            int finalMoney = money;
             while(calMonthcheck(money,rate,countMonth) < money * 2){
-                finalMoney+=calMonthcheck(finalMoney,rate,countMonth+1);
                 countMonth++;
             }
             return countMonth;
         }
+
         static int calMonthBasic(int money, int rate){
             int countMonth = 0;
             int finalMoney = money;
@@ -29,8 +29,8 @@ namespace bai22calMonth
 
         static void Main(string[] args)
         {
-            Console.WriteLine("It takes: "  + calMonthBasic(100000,20) + " months to double the money with 10% interest with basic");
-            Console.WriteLine("It takes: "  + calMonthRecursion(100000,20) + " months to double the money with 10% interest with basic");
+            Console.WriteLine("It takes: "  + calMonthBasic(10000,20) + " months to double the money with 10% interest with basic");
+            Console.WriteLine("It takes: "  + calMonthRecursion(10000,20) + " months to double the money with 10% interest with basic");
         }
     }
 }
